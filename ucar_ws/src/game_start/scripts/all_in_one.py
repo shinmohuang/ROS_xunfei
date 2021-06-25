75e01c7f489d75899e69febbf5a951ff295cbb13
ADAM HUANG <68174921+shinmohuang@users.noreply.github.com>
2021-06-26 00:20:11 +0800

    Update all_in_one.py
    add smach things

--- ucar_ws/src/game_start/scripts/all_in_one.py
+++ ucar_ws/src/game_start/scripts/all_in_one.py
@@ -10,4 +10,24 @@ from rospy.topics import Poller
 from smach import StateMachine
 from smach_ros import SimpleActionState
 
-# class VOICE_AWAKE(SimpleActionState):
+import patrol_B
+import patrol_D1
+
+class Wait(smach.State):
+    def __init__(self):
+        smach.State.__init__(self, 
+                             outcomes=['navigating', 'wait', 'end'])
+    
+    def execute(self, userdata):
+        global g_targets, g_start
+        if rospy.is_shutdown():
+            return 'end'
+
+        if g_start == True:
+            if len(g_targets) > 0:
+                return 'navigating'
+            else:
+                print('No targets, can not start.')
+                return 'wait'
+        else:
+            return 'wait'
